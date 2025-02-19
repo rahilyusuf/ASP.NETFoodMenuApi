535c09842c7a923fac94a41fdf2ba6339219c4a5
Rahil Yusuf <Rahil.Yusuf@raasoft.com>
2025-02-19 11:08:43 +0530

    inlcuded gitignorefile

--- obj/Debug/net8.0/FoodMenuApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/FoodMenuApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodMenuApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ac5dbdc90392b31a3db3da6f0a804abaf8d276")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodMenuApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodMenuApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
